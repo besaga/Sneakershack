4c4d37057b0a797f95ac2e3ae2578ce6bdc0087e
Analia Lopez <analialopezrodriguez@gmail.com>
2021-12-11 12:34:08 +0100

    sin nada

--- server/config/session.config.js
+++ server/config/session.config.js
@@ -4,16 +4,15 @@ const MongoStore = require('connect-mongo');
 module.exports = app => {
   app.use(
     session({
-      secret: "sneakers",//process.env.SESS_SECRET,
+      secret: "sneakers",
       resave: true,
       saveUninitialized: false,
       cookie: {
-        // sameSite: 'none',
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24
       },
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI ||'mongodb+srv://sneakershack:sneakershack@cluster0.f83if.mongodb.net/test'
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/server'
       })
     })
   );
