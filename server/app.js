36fcfd1713855d7e3b0c57266321d2346a3cec7c
Analia Lopez <analialopezrodriguez@gmail.com>
2021-12-16 13:37:04 +0100

    deploy

--- server/app.js
+++ server/app.js
@@ -5,13 +5,10 @@ const path = require('path')
 const express = require("express");
 const app = express();
 
+require("./config")(app);
+require("./config/session.config")(app)
+
 app.use(express.static(path.join(__dirname, "public")))
 require("./routes")(app)
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
-
-require("./config")(app);
-require("./config/session.config")(app)
-require("./routes")(app)
-require("./error-handling")(app);
-console.log()
 module.exports = app;
